11f728d8220ccb754f2d8949e2f02096f2e861df
Jam Hejrati <jamhejratipro@gmail.com>
2023-07-09 17:52:33 +0330

    changed to react-konva package

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,47 @@
-import './App.css'
-import Fishbone from "@hophiphip/react-fishbone";
+import { Box } from "@mui/material";
+import "./App.css";
+import { Layer, Line, Stage } from "react-konva";
+import { useEffect, useState } from "react";
 
 function App() {
+    const [stageSize, setStageSize] = useState({ width: window.innerWidth, height: window.innerHeight });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setStageSize({ width: window.innerWidth, height: window.innerHeight });
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
     return (
-        <>
-            <Fishbone
-                items={{
-                    "name": "Flaws",
-                    "children": [
-                        {
-                            "name": "Machines",
-                            "children": [
-                                {"name": "Speed"},
-                                {"name": "Bits"},
-                                {"name": "Sockets"}
-                            ]
-                        },{
-                            "name": "Machines",
-                            "children": [
-                                {"name": "Speed"},
-                                {"name": "Bits"},
-                                {"name": "Sockets"}
-                            ]
-                        },{
-                            "name": "Machines",
-                            "children": [
-                                {"name": "Speed"},
-                                {"name": "Bits"},
-                                {"name": "Sockets"}
-                            ]
-                        }
-                    ]
-                }}
-                wrapperStyle={{
-                    width: 1200,
-                    height: 500,
-                }}
-            />
-        </>
-    )
+        <Box sx={{ width: "100%", height: "100%" }}>
+            <Stage width={stageSize.width} height={stageSize.height}>
+                <Layer>
+                    <Line
+                        x={100}
+                        y={-19}
+                        points={[0, stageSize.height / 2, stageSize.width - 300, stageSize.height / 2]}
+                        tension={0.5}
+                        closed
+                        stroke="black"
+                    />
+                    <Line
+                        x={100}
+                        y={stageSize.height / 2 - 20}
+                        points={[150, -210, 250, 0]}
+                        tension={0.5}
+                        closed
+                        stroke="red"
+                    />
+                </Layer>
+            </Stage>
+        </Box>
+    );
 }
 
-export default App
+export default App;
