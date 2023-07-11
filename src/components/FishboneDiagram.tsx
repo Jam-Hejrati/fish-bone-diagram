27026e4fba03f4a3f5afdb4164dba800fcc2b742
Jam Hejrati <jamhejratipro@gmail.com>
2023-07-11 18:06:38 +0330

    fix: position of every line's text

--- src/components/FishboneDiagram.tsx
+++ src/components/FishboneDiagram.tsx
@@ -1,6 +1,6 @@
 import { Box } from "@mui/material";
 import { useEffect, useState } from "react";
-import { Layer, Line, Stage } from "react-konva";
+import { Layer, Line, Stage, Text } from "react-konva";
 
 type Line = {
     x1: number;
@@ -33,7 +33,7 @@ const FishboneDiagram = () => {
     const SpineCordinate = {
         x1: 30,
         y1: stageSize.height / 2,
-        x2: stageSize.width - 30,
+        x2: stageSize.width - 250,
         y2: stageSize.height / 2,
     };
 
@@ -65,16 +65,13 @@ const FishboneDiagram = () => {
             title: "نیروانسانی",
             children: [
                 {
-                    title: "علل یک",
+                    title: "قصه حسین کرد تا فردا صبح کش میایه",
                     children: [
                         {
-                            title: "علل 2",
-                        },
-                        {
-                            title: "علل 1.2",
+                            title: "علل نیبت ینتب ینت بنیتب نی1.2",
                             children: [
                                 {
-                                    title: "علل 2",
+                                    title: "عبتین تنیبتینت بیبت ینت بینلل 2",
                                     children: [
                                         {
                                             title: "علل 2",
@@ -84,6 +81,24 @@ const FishboneDiagram = () => {
                             ],
                         },
                     ],
+                },
+                {
+                    title: "قصه حسین کرد تا فردا صبح کش میایه",
+                    // children: [
+                    //     {
+                    //         title: "علل نیبت ینتب ینت بنیتب نی1.2",
+                    //         children: [
+                    //             {
+                    //                 title: "عبتین تنیبتینت بیبت ینت بینلل 2",
+                    //                 children: [
+                    //                     {
+                    //                         title: "علل 2",
+                    //                     },
+                    //                 ],
+                    //             },
+                    //         ],
+                    //     },
+                    // ],
                 },
             ],
         },
@@ -178,22 +193,22 @@ const FishboneDiagram = () => {
         // },
     ];
 
-    const reflecter = (lineTree, YSpinecoordinate) => {
-        lineTree.forEach((item) => {
-            if (item.coord) {
-                item.coord[1] = YSpinecoordinate - item.coord[1] + YSpinecoordinate;
-                item.coord[3] = YSpinecoordinate - item.coord[3] + YSpinecoordinate;
-            }
-            if (item.children) {
-                reflecter(item.children, YSpinecoordinate);
-            }
-        });
+    const reflecter = (item: any, YSpinecoordinate: number) => {
+        // lineTree.forEach((item) => {
+        if (item.coord) {
+            item.coord[1] = YSpinecoordinate - item.coord[1] + YSpinecoordinate;
+            item.coord[3] = YSpinecoordinate - item.coord[3] + YSpinecoordinate;
+        }
+        if (item.children) {
+            reflecter(item.children, YSpinecoordinate);
+        }
+        // });
     };
 
-    const items = [];
+    const items: any[] = [];
     let depthCounter = 1;
     const createCordinates = (data: any, makeNewLine: any, cord: any) => {
-        data.forEach((item: any, index: number) => {
+        data.forEach((item: any) => {
             if (!cord) {
                 item.cord = makeNewLine(SpineCordinate, 400, Math.PI / 2.3, 0.4);
             } else {
@@ -221,26 +236,34 @@ const FishboneDiagram = () => {
                         depthCounter++;
                         createCordinates(item.children, makeNewLine, cord); // Recursively call the function for nested children
                     } else {
-                        console.log(cord);
                         depthCounter++;
                         createCordinates(item.children, makeNewLine, item.cord); // Recursively call the function for nested children
                     }
                 }
             }
-            console.log(item);
             items.push({ title: item?.title, coord: item?.cord });
         });
     };
 
     createCordinates(fishboneData, makeNewLine, null);
-    reflecter(items, stageSize.height / 2);
+    // items.forEach((item , index) => {
+    //     if(index % 2 === 0) reflecter(item, stageSize.height / 2);
+    // })
 
     const randomColorGenerator = () => {
         const color = Math.floor(Math.random() * 16777215).toString(16);
-        console.log(color);
         return color;
     };
-    console.log(makeNewLine(SpineCordinate, 200, Math.PI / 2.3, 0.85));
+
+    const getTextWidth = (text: string, fontSize: number, fontFamily: string) => {
+        const tempText = new window.Konva.Text({
+            text,
+            fontSize,
+            fontFamily,
+        });
+        return tempText.getTextWidth();
+    };
+    console.log(getTextWidth("djfkdljfdkl", 16, "sans"));
     return (
         <Box
             sx={{
@@ -249,6 +272,7 @@ const FishboneDiagram = () => {
                 display: "flex",
                 alignItems: "center",
                 justifyContent: "center",
+                direction: "rtl",
             }}
         >
             <Stage width={stageSize.width} height={stageSize.height}>
@@ -259,15 +283,38 @@ const FishboneDiagram = () => {
                         closed
                         stroke="black"
                         strokeWidth={3}
-                    />  
+                    />
+                    <Text
+                        x={SpineCordinate.x2 + 8}
+                        y={SpineCordinate.y2 - 10}
+                        text="عدم رضایت مشتریان از ارائه فاکتور"
+                        fontSize={20}
+                        fontFamily="IranSans"
+                        fill="black"
+                    />
                     {items.map((item) => (
-                        <Line
-                            points={item.coord}
-                            tension={0.5}
-                            closed
-                            stroke={`#${randomColorGenerator()}`}
-                            strokeWidth={3}
-                        />
+                        <>
+                            <Line
+                                points={item.coord}
+                                stroke={`#${randomColorGenerator()}`}
+                                strokeWidth={3}
+                                // onClick={() => console.log("text")}
+                            />
+                            <Text
+                                x={
+                                    item.coord[2] -
+                                    (getTextWidth(item.title, 20, "Arial") > 200
+                                        ? 150
+                                        : getTextWidth(item.title, 20, "Arial") + 25)
+                                }
+                                y={item.coord[3] - 20}
+                                text={item.title}
+                                fontSize={20}
+                                fontFamily="IranSans"
+                                width={150}
+                                fill="black"
+                            />
+                        </>
                     ))}
                 </Layer>
             </Stage>
